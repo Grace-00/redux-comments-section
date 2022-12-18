7af75f3dd65e1dce6b56aac3ca773fbf06b93bac
Grazia Palombella <grazia.palombella@gmail.com>
2022-12-18 16:09:30 +0100

    redux setup, eslint fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import EntryPoint from "./EntryPoint"
+import { store } from './store'
+import { Provider } from 'react-redux'
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <EntryPoint />
+    <Provider store={ store }>
+      <EntryPoint />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 )
