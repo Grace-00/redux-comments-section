1155648afc8b64d65fbc629de00a5ca566293757
Grazia Palombella <grazia.palombella@gmail.com>
2022-12-23 23:28:56 +0100

    add tag for currentUser

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useAppSelector } from '../../redux/selectors'
 import { User } from '../../redux/slices/commentsSlice'
 import './user.css'
 
@@ -9,10 +10,18 @@ interface UserProps {
 }
 
 const User = (props: UserProps) => {
+  const currentUser = useAppSelector(state => state.data?.currentUser.username)
+
+  const isCurrentUser = (username: string) => {
+    return currentUser === username ? 
+    (<p className='user-name'>{username} <span className='currentUser-tag'>you</span></p>) : 
+    (<p className='user-name'>{username}</p>)
+  }
+
   return (
     <div className='user'>
       <img src={`./image-${props.user.username}.png`} alt={props.user.username} className='user-img'/>
-      <p className='user-name'>{props.user.username}</p>
+      {isCurrentUser(props.user.username)}
       <p>{props.createdAt}</p>
     </div>
   )
